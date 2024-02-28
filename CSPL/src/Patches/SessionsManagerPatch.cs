37ab86057519f54d3b4afdb6363a6ed47d2488c2
v0idp <voidp@protonmail.com>
2024-02-28 02:02:20 +0100

    Accidently disabled cloud saving on shared save. Attempted share upload will now properly result in cloudsaving instead

--- CSPL/src/Patches/SessionsManagerPatch.cs
+++ CSPL/src/Patches/SessionsManagerPatch.cs
@@ -10,10 +10,10 @@ namespace CSPL.Patches
         [HarmonyPatch(typeof(SessionsManager), nameof(SessionsManager.SaveSessionAs))]
         public static void SaveSessionAs_Prefix(SessionsManager __instance, ref SessionSaveData sessionSaveData)
         {
-            if (sessionSaveData.IsShared || sessionSaveData.IsCloud)
+            if (sessionSaveData.IsShared)
             {
                 sessionSaveData.IsShared = false;
-                sessionSaveData.IsCloud = false;
+                sessionSaveData.IsCloud = true;
                 CSPLPlugin.Log.LogInfo("Preventing session from being shared to community...");
                 CSPLPlugin.Log.LogInfo("Session will be uploaded to cloud instead!");
             }
